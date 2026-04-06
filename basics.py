1f5144ee2d721f2344be29d7c6e50d78c6b4dec4
susmi-skariya <susmiskariya329@gmail.com>
2026-04-06 11:33:30 +0530

    basics of python

---
+++ basics.py
@@ -0,0 +1,63 @@
+# a=10
+# a=15  
+# print(a)  // delete the first value of a  (variables can starts with _,alphabets)
+
+# Data Types
+# a=5
+# print(type(a))
+# a="susmi"
+# print(type(a))
+# a=True
+# print(type(a))
+# a=(10,20,30)
+# print(type(a))  # Tuple (Immutable : Cant change ITEMS)
+# a=[10,20,30,0]   
+# print(type(a))  # List (Mutable : Can change ITEMS)
+
+# a={
+#     "name":"Aami",
+#     "age":"20"
+# }
+# print(type(a))   # Dictionary
+
+
+# a={1,2,3,3,4,4,4,5,6}
+# print(a)     # SET (Never take DUPLICATE values)
+# print(type(a))
+
+
+
+# Operator
+# a=5
+# b=5
+# print(a==b) #Comparison
+# print(a+5)
+# print(a-5)
+# print(a*5) 
+# print(a**2) #square
+# print(a%5)
+# print(a/6) #Division
+# print(a//6)  #FloorDivision
+
+# Assignment Operator
+# a=5
+# a+=10
+# a-=4
+# a*=2
+# a/=3
+# print(a)
+
+# Logical Operator
+a=20
+b=30
+print(a>b and b>a)
+print(a<b and b>a)
+print(a>b or b>a)
+print(not(a<=b))
+print(not(a>=b))
+
+
+
+
+
+
